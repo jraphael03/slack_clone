9dd2debda8308283dc5a0227e4c66fcc49a0ef0e
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-02-25 13:01:10 -0500

    Completed Header component

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -35,14 +35,12 @@ export default App;
 const Container = styled.div`
   width: 100%;
   height: 100vh;   
-  background: orange; 
   display: grid;
   grid-template-rows: 38px auto;    // define height of rows Container 38px then Main auto     This is how we got the heights
 
 `
 
 const Main = styled.div`
-  background: blue;
   display: grid;
   grid-template-columns: 260px auto; // define width of columns Main 260px then Sidebar auto     This is how we got the widths
 `;
--- slack-clone/src/components/Header.js
+++ slack-clone/src/components/Header.js
@@ -1,11 +1,103 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <Container>
+            <Main>
+                <AccessTimeIcon />
+                <SearchContainer>
+                    <Search>
+                        <input type="text" placeholder="Search..." />
+                    </Search>
+                </SearchContainer>
+                <HelpOutlineIcon />
+
+            </Main>
+            <UserContainer>
+                <Name>
+                    Justin
+                </Name>
+                <UserImage>
+                    <img src="/images/img1.jpg" alt="User"/>            {/* Image is held in the public folder */}
+                </UserImage>
+            </UserContainer>
+        </Container>
     )
 }
 
 export default Header
+
+const Container = styled.div`
+  background: #540d36;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const Main = styled.div`
+    display: flex;      // Layouts items inside of the div
+    margin-right: 16px;
+    margin-left: 16px;
+`
+
+const SearchContainer = styled.div`
+    min-width: 400px;
+    margin-left: 16px;
+    margin-right: 16px;
+    
+`
+
+const Search = styled.div`
+  box-shadow: inset 0 0 0 1px rgb(104 74 104);
+  width: 100%;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+
+  input {
+    // target input inside of the Search component
+    background: transparent;
+    border: none;
+    padding-left: 8px;
+    padding-right: 8px;
+    color: #ffffff;
+    width: 100%;
+    padding-top: 4px;
+    padding-bottom: 4px;
+  }
+
+  input:focus{              // When you click on the search contianer and the blue border pops up
+      outline: none;
+  }
+
+`;
+
+const UserContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 16px;
+    position: absolute;     // position relative in the container
+    right: 0;
+`
+
+const Name = styled.div`
+  padding-right: 16px;
+`
+
+const UserImage = styled.div`
+  width: 28px;
+  height: 28px;
+  border: 2px solid white;
+  border-radius: 3px;
+
+  img{
+      width: 100%;
+      height: 100%;
+  }
+
+`
