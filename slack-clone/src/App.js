45c71df60d726a14454fa06bc89c75e99eb09e69
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-02-25 00:21:29 -0500

    Creating architecture of slack_clone

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,21 +1,48 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
+import Chat from './components/Chat';
+import Login from './components/Login';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/room">
-            Chat Page
-          </Route>
-          <Route path="/">      {/* Login page will be main page */}
-              Login page
-          </Route>
-        </Switch>
+        <Container>
+          {/* Style everything inside of it */}
+          <Header />
+          <Main>
+            <Sidebar />
+            <Switch>
+              <Route path="/room">
+                <Chat /> {/* Chat Component */}
+              </Route>
+              <Route path="/">      {/* Login page will be main page */}
+                <Login />
+              </Route>
+            </Switch>
+          </Main>
+        </Container>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;   
+  background: orange; 
+  display: grid;
+  grid-template-rows: 38px auto;    // define height of rows Container 38px then Main auto     This is how we got the heights
+
+`
+
+const Main = styled.div`
+  background: blue;
+  display: grid;
+  grid-template-columns: 260px auto; // define width of columns Main 260px then Sidebar auto     This is how we got the widths
+`;
